6f6f4254496a2408f8970d80f0f089375e73602c
TarekElkabany <126732712+TarekElkabany@users.noreply.github.com>
2023-03-06 16:38:50 +0200

    Add files via upload

---
+++ stop_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# Check if the server process is running
+
+if pgrep -x "nginx" > /dev/null; then
+
+    # Stop the server process
+
+    systemctl stop nginx
+
+    echo "Server stopped successfully.
+"
+else
+    echo "Server is not running.
+"
+fi
+
---
+++ validate_service.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+# Check if the response contains the expected string
+
+curl -s http://44.199.194.151 | grep "Welcome"
